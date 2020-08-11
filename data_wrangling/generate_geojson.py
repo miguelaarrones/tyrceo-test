bfd1cbbe95c574144ad883f7664e3fab818f4f7d
Miguel Angel Arrones <miguelaarrones@icloud.com>
2020-08-11 18:50:28 +0200

    GeoJson generation done and data visualization with MapBox

---
+++ data_wrangling/generate_geojson.py
@@ -0,0 +1,30 @@
+import pymysql.cursors
+import geojson
+
+connection = pymysql.connect(host='35.187.55.190',
+                             user='candidate',
+                             password='Fbps9Y7MhKQa4XPxjYo8',
+                             db='test',
+                             charset="utf8mb4",
+                             cursorclass=pymysql.cursors.DictCursor)
+try:
+    with connection.cursor() as cursor:
+        sql = "SELECT * FROM `data` INNER JOIN `data_at_coordinate` ON `data`.id = `data_at_coordinate`.id_data " \
+              "INNER JOIN `coordinates` ON `coordinates`.id = `data_at_coordinate`.id_coordinate " \
+              "WHERE `score` >= 0.5 AND (`pokemon` SOUNDS LIKE 'Bulbasaur' " \
+              "OR `pokemon` SOUNDS LIKE 'Charmander' OR `pokemon` SOUNDS LIKE 'Squirtle')"
+        cursor.execute(sql)
+        result = cursor.fetchall()
+
+        features = []
+        for row in result:
+            point = geojson.Point((row['longitude'], row['latitude']))
+            features.append(geojson.Feature(geometry=point, properties={"Pokemon": row['pokemon'].upper(),
+                                                                        "Score": row['score']}))
+
+        feature_collection = geojson.FeatureCollection(features)
+        with open('points.geojson', 'w') as f:
+            geojson.dump(feature_collection, f)
+
+finally:
+    connection.close()
